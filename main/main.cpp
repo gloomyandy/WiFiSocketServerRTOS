d4a49f324197228c9854dfd65c1a273a9906e138
rechrtb <rechrtb@gmail.com>
2022-01-21 09:03:36 +0800

    Give more CPU time to main task

--- main/main.cpp
+++ main/main.cpp
@@ -7,14 +7,26 @@
 
 extern void setup();
 extern void loop();
+extern "C" unsigned long millis();
+
+void yield()
+{
+    static unsigned long last = 0;
+    static constexpr unsigned long wait =  0.8f * CONFIG_ESP_TASK_WDT_TIMEOUT_S * 1000; 
+    unsigned long now = millis();
+
+    if(now - last >= wait) {
+        last = now;
+        vTaskDelay(2);
+    }
+}
 
 extern "C" void app_main()
 {
-    vTaskPrioritySet(NULL, ESP_TASK_PRIO_MIN);
+    vTaskPrioritySet(NULL, ESP_TASK_PRIO_MIN + 1);
     setup();
-    taskYIELD();
     while (true) {
         loop ();
-        taskYIELD();
+        yield();
     }
 }
