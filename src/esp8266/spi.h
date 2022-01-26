5de0a5f3848165979345ee4393ce0db03081e0ae
rechrtb <rechrtb@gmail.com>
2022-01-26 10:18:40 +0800

    spi: define registers using rtos sdk

--- src/esp8266/spi.h
+++ src/esp8266/spi.h
@@ -1,31 +1,32 @@
 #pragma once
 
 #include "driver/gpio.h"
+#include "esp8266/spi_register.h"
 
 static const gpio_num_t SCK   = GPIO_NUM_14;
 static const gpio_num_t MOSI  = GPIO_NUM_13;
 static const gpio_num_t MISO  = GPIO_NUM_12;
 
-#define ESP8266_REG(addr) *((volatile uint32_t *)(0x60000000+(addr)))
+#define REG(addr)   (*((volatile uint32_t *) addr))
 
-#define SPI1C       ESP8266_REG(0x108)
-#define SPICWBO     (1 << 26) //SPI_WR_BIT_ODER
-#define SPICRBO     (1 << 25) //SPI_RD_BIT_ODER
-#define SPI1U       ESP8266_REG(0x11C)
-#define SPI1U1      ESP8266_REG(0x120)
-#define SPIUMOSI    (1 << 27) //MOSI phase, SPI_FLASH_DOUT
-#define SPIUDUPLEX  (1 << 0) //SPI_DOUTDIN
-#define SPILMOSI    17 //9 bit in SPIxU1 default:0 (1bit)
-#define SPILMISO    8  //9 bit in SPIxU1 default:0 (1bit)
-#define SPI1C1      ESP8266_REG(0x10C)
-#define SPI1S       ESP8266_REG(0x130)
-#define SPIUSME     (1 << 7) //SPI Master Edge (0:falling, 1:rising), SPI_CK_OUT_EDGE
-#define SPIUSSE     (1 << 6) //SPI Slave Edge (0:falling, 1:rising), SPI_CK_I_EDGE
-#define SPI1P       ESP8266_REG(0x12C)
-#define SPI1CMD     ESP8266_REG(0x100)
-#define SPIBUSY     (1 << 18) //SPI_USR
-#define GPMUX       ESP8266_REG(0x800)
-#define SPI1CLK 	ESP8266_REG(0x118)
-#define SPIMMISO	0x1FF
-#define SPIMMOSI	0x1FF
-#define SPI1W0 		ESP8266_REG(0x140)
+#define SPI1C       REG(SPI_CTRL(1))
+#define SPICWBO     SPI_WR_BIT_ORDER //SPI_WR_BIT_ODER
+#define SPICRBO     SPI_RD_BIT_ORDER //SPI_RD_BIT_ODER
+#define SPI1U       REG(SPI_USER(1))
+#define SPI1U1      REG(SPI_USER1(1))
+#define SPIUMOSI    SPI_USR_MOSI //MOSI phase, SPI_FLASH_DOUT
+#define SPIUDUPLEX  BIT0 //SPI_DOUTDIN
+#define SPILMOSI    SPI_USR_MOSI_BITLEN_S //9 bit in SPIxU1 default:0 (1bit)
+#define SPILMISO    SPI_USR_MISO_BITLEN_S  //9 bit in SPIxU1 default:0 (1bit)
+#define SPI1C1      REG(SPI_CTRL1(1))
+#define SPI1S       REG(SPI_SLAVE(1))
+#define SPIUSME     SPI_CK_OUT_EDGE //SPI Master Edge (0:falling, 1:rising), SPI_CK_OUT_EDGE
+#define SPIUSSE     SPI_CK_I_EDGE //SPI Slave Edge (0:falling, 1:rising), SPI_CK_I_EDGE
+#define SPI1P       REG(SPI_PIN(1))
+#define SPI1CMD     REG(SPI_CMD(1))
+#define SPIBUSY     SPI_USR //SPI_USR
+#define GPMUX       REG(PERIPHS_IO_MUX)
+#define SPI1CLK 	REG(SPI_CLOCK(1))
+#define SPIMMISO	SPI_USR_MISO_BITLEN
+#define SPIMMOSI	SPI_USR_MOSI_BITLEN
+#define SPI1W0 		REG(SPI_W0(1))
