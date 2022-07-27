3f36eef0b735373ceb33d8332736c6553eb8b628
rechrtb <rechrtb@gmail.com>
2022-07-27 13:33:15 +0800

    Fix some issues

--- src/WirelessConfigurationMgr.cpp
+++ src/WirelessConfigurationMgr.cpp
@@ -6,10 +6,10 @@ WirelessConfigurationMgr* WirelessConfigurationMgr::instance = nullptr;
 
 void WirelessConfigurationMgr::Init()
 {
-	nvs_flash_init_partition(SSIDS_STORAGE_NS);
-	nvs_open_from_partition(SSIDS_STORAGE_NS, SSIDS_STORAGE_NS, NVS_READWRITE, &ssidsStorage);
+	nvs_flash_init_partition(SSIDS_STORAGE_NAME);
+	nvs_open_from_partition(SSIDS_STORAGE_NAME, SSIDS_STORAGE_NAME, NVS_READWRITE, &ssidsStorage);
 
-	nvs_iterator_t savedSsids = nvs_entry_find(SSIDS_STORAGE_NS, SSIDS_STORAGE_NS, NVS_TYPE_ANY);
+	nvs_iterator_t savedSsids = nvs_entry_find(SSIDS_STORAGE_NAME, SSIDS_STORAGE_NAME, NVS_TYPE_ANY);
 	if (!savedSsids) {
 		FactoryReset();
 
@@ -38,6 +38,14 @@ void WirelessConfigurationMgr::Init()
 	nvs_release_iterator(savedSsids);
 
 #if ESP32C3
+	credsScratch = esp_partition_find_first(ESP_PARTITION_TYPE_DATA,
+		ESP_PARTITION_SUBTYPE_DATA_NVS, SCRATCH_STORAGE_NAME);
+
+	spi_flash_mmap_handle_t  mapHandle;
+	// Memory map the partition. The base pointer will be returned.
+	esp_partition_mmap(credsScratch, 0, credsScratch->size, SPI_FLASH_MMAP_DATA, reinterpret_cast<const void**>(&scratchBase), &mapHandle);
+
+
     // Storing an enterprise SSID might not have gone all the way.
     // The SSID information is written last, after all of the credentials;
     // so there might be orphaned credentials taking up space. Clear them here.
@@ -68,16 +76,14 @@ void WirelessConfigurationMgr::FactoryReset()
 	}
 }
 
-
-
 #if ESP32C3
 nvs_handle_t WirelessConfigurationMgr::OpenCredentialStore(int ssid, bool write)
 {
 	char ssidCredsNs[NVS_KEY_NAME_MAX_SIZE] = { 0 };
-	snprintf(ssidCredsNs, sizeof(ssidCredsNs), CREDS_STORAGE_NS, ssid);
+	snprintf(ssidCredsNs, sizeof(ssidCredsNs), CREDS_STORAGE_NAME, ssid);
 
 	nvs_handle_t ssidCreds;
-	nvs_open_from_partition(SSIDS_STORAGE_NS, ssidCredsNs, write ? NVS_READWRITE : NVS_READONLY, &ssidCreds);
+	nvs_open_from_partition(SSIDS_STORAGE_NAME, ssidCredsNs, write ? NVS_READWRITE : NVS_READONLY, &ssidCreds);
 
 	return ssidCreds;
 }
@@ -112,60 +118,40 @@ size_t WirelessConfigurationMgr::GetCredential(int ssid, int cred, int chunk, vo
 
 const uint8_t* WirelessConfigurationMgr::LoadCredentials(int ssid, CredentialsInfo& offsets)
 {
-	// Erase scratch partition
-	static const esp_partition_t* scratch = nullptr;
-	static const uint8_t* base = nullptr;
+	size_t offset = 0;
+	esp_err_t err = esp_partition_erase_range(credsScratch, offset, credsScratch->size);
 
-	if (!scratch)
+	if (err == ESP_OK)
 	{
-		scratch = esp_partition_find_first(ESP_PARTITION_TYPE_DATA,
-			ESP_PARTITION_SUBTYPE_DATA_NVS, "scratch");
-	}
+		// Store offsets from the base partition
+		uint32_t *offsetsArr = reinterpret_cast<uint32_t*>(&offsets);
+		uint8_t *buff = static_cast<uint8_t*>(malloc(MaxCredentialChunkSize));
 
-	if (scratch)
-	{
-		if (!base)
+		for(int cred = 0; cred < sizeof(offsets)/sizeof(offsetsArr[0]); cred++)
 		{
-			spi_flash_mmap_handle_t  mapHandle;
-			// Memory map the partition. The base pointer will be returned.
-			esp_partition_mmap(scratch, 0, scratch->size, SPI_FLASH_MMAP_DATA, (const void**)&base, &mapHandle);
-		}
+			offsetsArr[cred] = offset;
 
-		size_t offset = 0;
-		esp_err_t err = esp_partition_erase_range(scratch, offset, scratch->size);
-
-		if (err == ESP_OK)
-		{
-			// Store offsets from the base partition
-			uint32_t *offsetsArr = reinterpret_cast<uint32_t*>(&offsets);
-			uint8_t *buff = static_cast<uint8_t*>(malloc(MaxCredentialChunkSize));
-
-			for(int cred = 0; cred < sizeof(offsets)/sizeof(offsetsArr[0]); cred++)
+			for(int chunk = 0; ; chunk++)
 			{
-				offsetsArr[cred] = offset;
-
-				for(int chunk = 0; ; chunk++)
+				memset(buff, 0, MaxCredentialChunkSize);
+				size_t sz = 0;
+				if ((sz = GetCredential(ssid, cred, chunk, nullptr, 0)))
 				{
-					memset(buff, 0, MaxCredentialChunkSize);
-					size_t sz = 0;
-					if ((sz = GetCredential(ssid, cred, chunk, nullptr, 0)))
-					{
-						sz = GetCredential(ssid, cred, chunk, buff, sz);
-						esp_partition_write(scratch, offset, buff, sz);
-						offset += sz;
-					}
-					else
-					{
-						break;
-					}
+					sz = GetCredential(ssid, cred, chunk, buff, sz);
+					esp_partition_write(credsScratch, offset, buff, sz);
+					offset += sz;
+				}
+				else
+				{
+					break;
 				}
 			}
-
-			free(buff);
 		}
+
+		free(buff);
 	}
 
-	return base;
+	return scratchBase;
 }
 
 void WirelessConfigurationMgr::EraseCredentials(int ssid)
@@ -173,6 +159,7 @@ void WirelessConfigurationMgr::EraseCredentials(int ssid)
 	nvs_handle_t creds = OpenCredentialStore(ssid, true);
 	nvs_erase_all(creds);
 	nvs_commit(creds);
+	nvs_close(creds);
 }
 #endif
 
--- src/WirelessConfigurationMgr.h
+++ src/WirelessConfigurationMgr.h
@@ -21,37 +21,40 @@
 class WirelessConfigurationMgr
 {
 public:
-    static WirelessConfigurationMgr* GetInstance()
-    {
-        if (!instance)
-        {
-            instance = new WirelessConfigurationMgr();
-        }
-        return instance;
-    }
-    void Init();
-    void FactoryReset();
+	static WirelessConfigurationMgr* GetInstance()
+	{
+		if (!instance)
+		{
+			instance = new WirelessConfigurationMgr();
+		}
+		return instance;
+	}
+	void Init();
+	void FactoryReset();
 
-    bool GetSsidDataByIndex(int ssid, WirelessConfigurationData& data);
-    int GetSsidDataByName(const char* ssid, WirelessConfigurationData& data);
-    const uint8_t* LoadCredentials(int ssid, CredentialsInfo& offsets);
-    int FindEmptySsidEntry();
-    bool SetSsidData(int ssid, const WirelessConfigurationData& data);
-    void EraseCredentials(int ssid);
-    bool SetCredential(int ssid, int cred, int chunk, const void* buff, size_t sz);
-    bool EraseSsidData(int ssid);
+	bool GetSsidDataByIndex(int ssid, WirelessConfigurationData& data);
+	int GetSsidDataByName(const char* ssid, WirelessConfigurationData& data);
+	const uint8_t* LoadCredentials(int ssid, CredentialsInfo& offsets);
+	int FindEmptySsidEntry();
+	bool SetSsidData(int ssid, const WirelessConfigurationData& data);
+	void EraseCredentials(int ssid);
+	bool SetCredential(int ssid, int cred, int chunk, const void* buff, size_t sz);
+	bool EraseSsidData(int ssid);
 private:
-    std::string GetCredentialKey(int cred, int chunk);
-    nvs_handle_t OpenCredentialStore(int ssid, bool write);
-    size_t GetCredential(int ssid, int cred, int chunk, void* buff, size_t sz);
+	std::string GetCredentialKey(int cred, int chunk);
+	nvs_handle_t OpenCredentialStore(int ssid, bool write);
+	size_t GetCredential(int ssid, int cred, int chunk, void* buff, size_t sz);
 
-    static WirelessConfigurationMgr* instance;
+	static WirelessConfigurationMgr* instance;
 
-    static constexpr char SSIDS_STORAGE_NS[] = "ssids";
-    static constexpr char CREDS_STORAGE_NS[] = "creds_%d";
+	static constexpr char SSIDS_STORAGE_NAME[] = "ssids";
+	static constexpr char CREDS_STORAGE_NAME[] = "creds_%d";
+	static constexpr char SCRATCH_STORAGE_NAME[] = "scratch";
 
-    nvs_handle_t ssidsStorage;
-    const esp_partition_t* credsScratch;
+	nvs_handle_t ssidsStorage;
+	const esp_partition_t* credsScratch;
+
+	const uint8_t* scratchBase;
 };
 
 #endif /* SRC_WIFI_CONFIGURATION_MANAGER_H_ */
