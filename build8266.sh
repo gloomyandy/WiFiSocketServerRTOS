12e96f19a5ad5acb36c716f476c8db9e1aa3de13
Andy Shaw <andy-git@gloomy-place.com>
2026-05-28 15:41:22 +0100

    Changes to allow use of old build system

--- build8266.sh
+++ build8266.sh
@@ -5,6 +5,6 @@ export PATH="/opt/xtensa-lx106-elf/bin":$PATH
 python -m pip install --user -r ../ESP8266_RTOS_SDK//requirements.txt
 # build esp8266 version, note first make may fail if not running from an interactive shell
 if ! [ -f ./sdkconfig ]; then
-make
+make -f Makefile.esp8266.msys
 fi
-make
+make -f Makefile.esp8266.msys
